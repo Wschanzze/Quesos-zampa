caa755e4c708e9c96164f4cb2fcfa0e1f762bae1
Joshua Gonzalez <josuugonzalezz@gmail.com>
2026-06-17 12:59:18 -0300

    Fix: Inline image hosts config and remove problematic webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { imageHosts } from './image-hosts.config.mjs';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: true,
@@ -11,7 +9,24 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: imageHosts,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pixabay.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.rocket.new',
+      },
+    ],
   },
   async redirects() {
     return [
@@ -21,32 +36,6 @@ const nextConfig = {
         permanent: false,
       },
     ];
-  },
-  webpack(
-    config,
-    {
-      dev: dev
-    }
-  ) {
-    config.module.rules.push({
-      test: /\.(jsx|tsx)$/,
-      exclude: [/node_modules/],
-      use: [{
-        loader: '@dhiwise/component-tagger/nextLoader',
-      }],
-    });
-    if (dev) {
-      const ignoredPaths = (process.env.WATCH_IGNORED_PATHS || '')
-        .split(',')
-        .map((p) => p.trim())
-        .filter(Boolean);
-      config.watchOptions = {
-        ignored: ignoredPaths.length
-          ? ignoredPaths.map((p) => `**/${p.replace(/^\/+|\/+$/g, '')}/**`)
-          : undefined,
-      };
-    }
-    return config;
   },
 };
 
